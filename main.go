141c2b2786ab32749f9ce4df6fd3a79904fe7f80
tsubasaogawa <hurikake@gmail.com>
2024-04-02 22:57:27 +0900

    refactoring

--- main.go
+++ main.go
@@ -19,6 +19,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 )
 
+type Url struct {
+	old string
+	new string
+}
+
 var (
 	bucket, entryPath, dir, region string
 	overwrite, uploadS3            bool
@@ -44,40 +49,29 @@ func main() {
 	}
 	s3Client := s3.NewFromConfig(sdkConfig)
 
-	rFlickrATag := regexp.MustCompile(`<a.*href="https?://www\.flickr\.com/(?:photos/\w+/\d+/in/[^"]+|gp/\w+/\w+)"[^>]*>`)
-	rFlickrScriptTag := regexp.MustCompile(`<script.*src="//embedr.flickr.com/assets/client-code.js"[^>]*></script>`)
-	rFlickrUrl := regexp.MustCompile(`https?://\w+\.staticflickr\.com/[0-9a-zA-Z_/]+\.(?:jpg|jpeg|png|gif)`)
+	rFlickrImageUrl := regexp.MustCompile(`https?://\w+\.staticflickr\.com/[0-9a-zA-Z_/]+\.(?:jpg|jpeg|png|gif)`)
 
 	entryPath = flag.Arg(0)
 	if entryPath == "" {
 		log.Fatal("Arg is required")
 	}
 
-	f, err := os.Open(entryPath)
+	entryTextb, err := os.ReadFile(entryPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
+	entryText := string(entryTextb)
 
-	entryb, err := io.ReadAll(f)
-	entry := string(entryb)
-
-	flickrUrls := rFlickrUrl.FindAllString(entry, -1)
-	if flickrUrls == nil {
-		log.Println("Flickr url is not found")
+	flickrImageUrls := rFlickrImageUrl.FindAllString(entryText, -1)
+	if flickrImageUrls == nil {
+		log.Println("Flickr url is not in an entry")
 		os.Exit(0)
 	}
-	replaceUrlPairs := make([]string, len(flickrUrls)*2)
-	for i, url := range flickrUrls {
+
+	replaceUrlPairs := make([]Url, len(flickrImageUrls))
+	for i, url := range flickrImageUrls {
 		// up to s3
-		resp, err := http.Get(url)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer resp.Body.Close()
-		// To calculate image size, the program should read full data to memory
-		// because Flickr cannot return Content-Length header.
-		imgb, err := io.ReadAll(resp.Body)
+		imgb, err := getImageByteFromFlickr(url)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -86,34 +80,75 @@ func main() {
 			dir += "/"
 		}
 		key := dir + filepath.Base(url)
-		result, err := s3Client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
-			Bucket: aws.String(bucket),
-			Prefix: aws.String(key),
-		})
-		if err != nil {
+		if err = uploadToS3(s3Client, key, imgb); err != nil {
 			log.Fatal(err)
-		} else if uploadS3 && (overwrite || *result.KeyCount < 1) {
-			_, err = s3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-				Bucket:        aws.String(bucket),
-				Key:           aws.String(key),
-				Body:          bytes.NewBuffer(imgb),
-				ContentLength: aws.Int64(int64(len(imgb))),
-			}, s3.WithAPIOptions(v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware))
-			if err != nil {
-				log.Fatal(err)
-			}
 		}
 
 		// replace old flickr url to new s3 one
-		newUrl := "https://" + bucket + "/" + key
-		replaceUrlPairs[i*2] = url
-		replaceUrlPairs[i*2+1] = newUrl
+		replaceUrlPairs[i].old = url
+		replaceUrlPairs[i].new = "https://" + bucket + "/" + key
 	}
+	fmt.Print(replaceFlickrHtml(parseUrl(replaceUrlPairs), entryText))
+}
+
+func getImageByteFromFlickr(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return []byte{}, err
+	}
+	defer resp.Body.Close()
+
+	// To calculate image size, the program should read full data to memory
+	// because Flickr cannot return Content-Length header.
+	imgb, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return []byte{}, err
+	}
+
+	return imgb, nil
+}
+
+func uploadToS3(s3c *s3.Client, key string, imgb []byte) error {
+	if !uploadS3 {
+		return nil
+	}
+	list, err := s3c.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input{
+		Bucket: aws.String(bucket),
+		Prefix: aws.String(key),
+	})
+	if err != nil {
+		return err
+	} else if !overwrite || *list.KeyCount > 0 {
+		return nil
+	}
+
+	_, err = s3c.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket:        aws.String(bucket),
+		Key:           aws.String(key),
+		Body:          bytes.NewBuffer(imgb),
+		ContentLength: aws.Int64(int64(len(imgb))),
+	}, s3.WithAPIOptions(v4.SwapComputePayloadSHA256ForUnsignedPayloadMiddleware))
+
+	return err
+}
+
+func replaceFlickrHtml(replaceUrlPairs []string, text string) string {
+	rFlickrATag := regexp.MustCompile(`<a.*href="https?://www\.flickr\.com/(?:photos/\w+/\d+/in/[^"]+|gp/\w+/\w+)"[^>]*>`)
+	rFlickrScriptTag := regexp.MustCompile(`<script.*src="//embedr.flickr.com/assets/client-code.js"[^>]*></script>`)
+
 	replacer := strings.NewReplacer(replaceUrlPairs...)
-	fmt.Print(replacer.Replace(
+	return replacer.Replace(
 		rFlickrScriptTag.ReplaceAllString(
-			rFlickrATag.ReplaceAllString(entry, `<a tabindex="-1">`),
+			rFlickrATag.ReplaceAllString(text, `<a tabindex="-1">`),
 			"",
 		),
-	))
+	)
+}
+
+func parseUrl(urls []Url) []string {
+	parsed := make([]string, len(urls)*2)
+	for _, url := range urls {
+		parsed = append(parsed, url.old, url.new)
+	}
+	return parsed
 }
